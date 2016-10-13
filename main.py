d765ead95a33ef9ddce47f39a8636465e05a0410
noerten <darkstria@gmail.com>
2016-10-14 01:37:05 +0300

    added saving, retrieving, checking

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import datetime
 import sqlite3
+import sys
 import urllib.request
 from bs4 import BeautifulSoup
 
@@ -24,12 +25,10 @@ def scrape_betsafe(html, rooms):
         promo_desc = item.find(id='PromotionDescriptionText').string
         promo_link = item.a.get('href')
         promo_image_link = item.find(id='PromotionImage').get('src')
-        promo_added = datetime.datetime.now()
         promo_room = rooms['betsafe']
-        one_promo = [promo_title, promo_desc, promo_type, promo_link,
-                     promo_image_link, promo_added, promo_room]
+        one_promo = (promo_title, promo_desc, promo_type, promo_link,
+                     promo_image_link, promo_room)
         betsafe_promos.append(one_promo)
-        print(one_promo)
     return betsafe_promos
 
 def scrape_triobet(html, rooms):
@@ -39,16 +38,14 @@ def scrape_triobet(html, rooms):
     for item in section.find_all('li', class_='promotion-container'):
         if item['class'][1] == 'odds': promo_type = 'sports'
         else: promo_type = item['class'][1]
-        promo_title = item.find('h2').string,
-        promo_desc = item.find('p').string,
-        promo_link = 'https://www.triobet.com'+item.a.get('href'),
-        promo_image_link = item.img.get('data-src'),
-        promo_added = datetime.datetime.now(),
+        promo_title = item.find('h2').string
+        promo_desc = item.find('p').string
+        promo_link = 'https://www.triobet.com'+item.a.get('href')
+        promo_image_link = item.img.get('data-src')
         promo_room = rooms['triobet']
-        one_promo = [promo_title, promo_desc, promo_type, promo_link,
-                     promo_image_link, promo_added, promo_room]
+        one_promo = (promo_title, promo_desc, promo_type, promo_link,
+                     promo_image_link, promo_room)
         triobet_promos.append(one_promo)
-        print(one_promo)
     return triobet_promos
 
 def scrape_guts(html):
@@ -67,25 +64,33 @@ def scrape_guts(html):
             })
     return guts_promos
 
-promos_urls = {betsafe_promos_url: scrape_betsafe,
+promos_urls = {
+               betsafe_promos_url: scrape_betsafe,
                triobet_promos_url: scrape_triobet,
                #guts_promos_url: scrape_guts,
                }
 
 def create_tables():
+    rooms = (
+        ("betsafe",),
+        ("triobet",),
+        )
+
     conn = sqlite3.connect('pps.sqlite3')
     with conn:
+        conn.execute('pragma foreign_keys=ON')
         cur=conn.cursor()
-        cur.execute('CREATE TABLE IF NOT EXISTS promos\
-                    (id INTEGER PRIMARY KEY, added TEXT, type TEXT, title TEXT,\
-                    desc TEXT, link TEXT, image_link TEXT, room_id INTEGER,\
-                    FOREIGN KEY(room_id) REFERENCES rooms(id))')
         cur.execute('CREATE TABLE IF NOT EXISTS rooms\
                     (id INTEGER PRIMARY KEY, name TEXT, UNIQUE(name))')
-        cur.execute('INSERT OR IGNORE INTO rooms(name) VALUES ("betsafe")')
-        cur.execute('INSERT OR IGNORE INTO rooms(name) VALUES ("triobet")')
+        cur.execute('CREATE TABLE IF NOT EXISTS promos\
+                    (id INTEGER PRIMARY KEY, title TEXT, desc TEXT, type TEXT,\
+                    link TEXT, image_link TEXT,\
+                    added_at DATETIME DEFAULT CURRENT_TIMESTAMP,\
+                    room_id INTEGER NOT NULL, is_active INTEGER\
+                    DEFAULT 1, FOREIGN KEY(room_id) REFERENCES rooms(id))')
+        cur.executemany("INSERT OR IGNORE INTO rooms(name) VALUES(?)", rooms)
 
-def get_all_rooms():
+def get_rooms():
     conn = sqlite3.connect('pps.sqlite3')
     with conn:
         cur=conn.cursor()
@@ -97,20 +102,73 @@ def get_all_rooms():
                 rooms[row[1]] = row[0]
         return rooms
 
-def insert_promos():
-    pass
+def get_promos():
+    conn = sqlite3.connect('pps.sqlite3')
+    with conn:
+        cur=conn.cursor()
+        cur.execute("SELECT title, desc, type, link, image_link, room_id\
+                    FROM promos WHERE is_active = 1")
+        promos = cur.fetchall()
+        return promos
 
-
+def insert_promos(compared_promos):
+    new_promos = compared_promos[0]
+    inactive_promos = compared_promos[1]
+    inactive_ids = []
+    conn = sqlite3.connect('pps.sqlite3')
+    with conn:
+        cur=conn.cursor()
+        if new_promos:
+            cur.executemany('INSERT INTO promos (title, desc, type, link,\
+                            image_link, room_id) VALUES (?,?,?,?,?,?)', new_promos)
+        if inactive_promos:
+            cur.execute("SELECT id, title, desc, type, link, image_link, room_id\
+                        FROM promos WHERE is_active = 1")
+            rows = cur.fetchall()
+            promos_dict = {}
+            for row in rows:
+                promo_wo_id = (row[1], row[2], row[3], row[4], row[5], row[6])
+                promo_id = row[0]
+                promos_dict[promo_wo_id] = promo_id
+            for i in inactive_promos:
+                if i in promos_dict:
+                    inactive_ids.append(promos_dict[i])
+            inactive_ids = tuple(inactive_ids)
+            cur.execute('UPDATE promos SET is_active = 0 WHERE id IN {ii}'\
+                        .format(ii=inactive_ids))
+#dont know why this doent work, upperr one works            
+#            cur.executemany('UPDATE promos SET is_active = 0 WHERE id IN ?', inactive_ids)
+def compare_promos(base_promos, scraped_promos):
+    new_promos = []
+    inactive_promos = []
+    for i in scraped_promos:
+        if i not in base_promos:
+            new_promos.append(i)
+    for i in base_promos:
+        if i not in scraped_promos:
+            inactive_promos.append(i)
+#    print(inactive_promos)
+    return [new_promos, inactive_promos]
     
 def main():
-    promos = []
+    scraped_promos = []
     create_tables()
-    rooms = get_all_rooms()
-    print(rooms)
+    print('tables ok')
+    rooms = get_rooms()
     for url in list(promos_urls.keys()):
-        promos.append(promos_urls[url](get_html(url), rooms))
-    
-
+        i=promos_urls[url](get_html(url), rooms)
+        scraped_promos = scraped_promos+i
+        print(str(len(i))+' promos were scraped from '+url)
+#    print(scraped_promos)
+    print('in total '+str(len(scraped_promos))+' promos were scraped from '\
+          +str(len(promos_urls))+' websites')
+    base_promos = get_promos()
+    print('there are '+str(len(base_promos))+' active promotions in db')
+    #compared_promos equals to [new_promos, inactive_promos]
+    compared_promos = compare_promos(base_promos, scraped_promos)
+    print('there are '+str(len(compared_promos[0]))+' new promotions and '+\
+          str(len(compared_promos[1]))+' inactive promotions')
+    insert_promos(compared_promos)
 
 if __name__ == '__main__':
     main()
